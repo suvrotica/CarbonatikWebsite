1b2a5455dda4db5ebdd27735bec747ff52dd3171
suvrotica <suvrotica@gmail.com>
2024-07-14 06:48:09 +0530

    feat: Add navigation bar component to layout

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,16 @@
+<script>
+    let navItems = $state([
+      { href: '#about', text: 'About Us' },
+      { href: '#operations', text: 'Our Operations' },
+      { href: '#investor-relations', text: 'Investor Relations' },
+      { href: '#contact', text: 'Contact' }
+    ]);
+  </script>
+  
+  <nav class="bg-graphite text-white p-4 border border-black">
+    <ul class="flex space-x-4">
+      {#each navItems as item}
+        <li><a href={item.href} class="hover:text-goldmica">{item.text}</a></li>
+      {/each}
+    </ul>
+  </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
     import "../app.css";
+    import Nav from "$lib/Nav.svelte";
   </script>
-  
+  <Nav />
   <slot />
