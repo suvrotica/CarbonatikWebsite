15a41d52dd37c605a0bec1c8bfc1a971193e60ba
suvrotica <suvrotica@gmail.com>
2024-07-16 09:19:38 +0530

    seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	import { seo, config, socialMeta } from '$lib/global';
 	import Nav from '$lib/Nav.svelte';
 	import FontPreload from '$lib/FontPreload.svelte';
-	import type { LayoutData } from './$types';
 </script>
-
-<svelte:head>
-	<meta charset="utf-8" />
-	<title>{seo.title}</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content={seo.description} />
-	<meta name="keywords" content={seo.keywords} />
-	<link rel="canonical" href={config.siteUrl} />
-	<link rel="icon" href="./fav-icon.png" />
-
-	<meta property="og:title" content={socialMeta.ogTitle} />
-	<meta property="og:description" content={socialMeta.ogDescription} />
-	<meta property="og:image" content={config.logoUrl} />
-	<meta property="og:url" content={config.siteUrl} />
-	<meta property="og:type" content="website" />
-
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={socialMeta.twitterTitle} />
-	<meta name="twitter:description" content={socialMeta.twitterDescription} />
-	<meta name="twitter:image" content={config.logoUrl} />
-	<script type="application/ld+json">
-		{stringifiedJsonLd}
-	</script>
-</svelte:head>
 
 <FontPreload href="/Fonts/Noto/NotoSans-VariableFont_wdth,wght.ttf" crossorigin="anonymous" />
 <FontPreload
